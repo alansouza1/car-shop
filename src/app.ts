5f96e2c585c35023fba879507c024cc1e13cc379
alansouza1 <alansouza4001@gmail.com>
2026-03-16 15:27:06 -0300

    feat: integrate Swagger UI in app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 import carRouter from './routes/car';
 import errorHandler from './middlewares/error';
+import swaggerSpec from './swagger';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(carRouter);
 app.use(errorHandler);
 
