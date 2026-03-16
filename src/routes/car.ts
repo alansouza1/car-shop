606bf7a10126eb67530b6b36da74deaf9dac314b
alansouza1 <alansouza4001@gmail.com>
2026-03-16 15:27:10 -0300

    docs: add OpenAPI documentation to car routes

--- src/routes/car.ts
+++ src/routes/car.ts
@@ -9,10 +9,113 @@ const car = new CarModel();
 const carService = new CarService(car);
 const carController = new CarController(carService);
 
+/**
+ * @openapi
+ * /cars:
+ *   post:
+ *     description: Create a new car
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Car'
+ *     responses:
+ *       201:
+ *         description: Car created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Car'
+ */
 route.post('/cars', (req, res) => carController.create(req, res));
+
+/**
+ * @openapi
+ * /cars/{id}:
+ *   get:
+ *     description: Get a car by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Car found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Car'
+ *       404:
+ *         description: Car not found
+ */
 route.get('/cars/:id', (req, res) => carController.readOne(req, res));
+
+/**
+ * @openapi
+ * /cars:
+ *   get:
+ *     description: Get all cars
+ *     responses:
+ *       200:
+ *         description: List of cars
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Car'
+ */
 route.get('/cars', (req, res) => carController.read(req, res));
+
+/**
+ * @openapi
+ * /cars/{id}:
+ *   put:
+ *     description: Update a car by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Car'
+ *     responses:
+ *       200:
+ *         description: Car updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Car'
+ *       404:
+ *         description: Car not found
+ */
 route.put('/cars/:id', (req, res) => carController.update(req, res));
+
+/**
+ * @openapi
+ * /cars/{id}:
+ *   delete:
+ *     description: Delete a car by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Car deleted successfully
+ *       404:
+ *         description: Car not found
+ */
 route.delete('/cars/:id', (req, res) => carController.delete(req, res));
 
 export default route;
