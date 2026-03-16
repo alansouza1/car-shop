66ff4606adf45b1ed12afb57bc2e2fb2650aef08
alansouza1 <alansouza4001@gmail.com>
2026-03-16 15:27:04 -0300

    refactor: update CarController to extend BaseController

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -1,51 +1,9 @@
-import { Request, Response } from 'express';
 import IService from '../interfaces/IService';
 import { ICar } from '../interfaces/ICar';
+import BaseController from './BaseController';
 
-export default class CarController {
-  constructor(private _service: IService<ICar>) { }
-
-  public async create(
-    req: Request,
-    res: Response<ICar>,
-  ) {
-    const { status, model, year, color, buyValue, doorsQty, seatsQty } = req.body;
-    const car = { status, model, year, color, buyValue, doorsQty, seatsQty };
-    const results = await this._service.create(car);
-    return res.status(201).json(results);
-  }
-
-  public async read(
-    req: Request,
-    res: Response<ICar[]>,
-  ) {
-    const result = await this._service.read();
-    return res.status(200).json(result);
-  }
-
-  public async readOne(
-    req: Request,
-    res: Response<ICar>,
-  ) {
-    const result = await this._service.readOne(req.params.id);
-    return res.status(200).json(result);
-  }
-
-  public async update(
-    req: Request,
-    res: Response<ICar>,
-  ) {
-    const { status, model, year, color, buyValue, doorsQty, seatsQty } = req.body;
-    const car = { status, model, year, color, buyValue, doorsQty, seatsQty };
-    const result = await this._service.update(req.params.id, car);
-    return res.status(200).json(result);
-  }
-
-  public async delete(
-    req: Request,
-    res: Response<ICar>,
-  ) {
-    await this._service.delete(req.params.id);
-    return res.status(204).end();
+export default class CarController extends BaseController<ICar> {
+  constructor(service: IService<ICar>) {
+    super(service);
   }
 }
